f21970438f457949b6fce327a82377742c8e0d99
dennis <dennis@semtri.com>
2016-10-31 22:34:24 +0900

    add icon field

--- models.py
+++ models.py
@@ -10,9 +10,16 @@ from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
 
 
+def content_file_name(instance, filename):
+    return '/'.join(['app_info', str(instance.pk), filename])
+        
+        
 class AppInfoExtension(PageExtension):
-    feature_image = models.ImageField(null=True, upload_to='features')
-    apple_store_id = models.CharField(null=True, max_length=256)
-    google_store_id = models.CharField(null=True, max_length=256)
+    feature_image = models.ImageField(null=True, blank=True, upload_to=content_file_name)
+    icon_image = models.ImageField(null=True, blank=True, upload_to=content_file_name)
+    apple_store_id = models.CharField(null=True, blank=True, max_length=256)
+    google_store_id = models.CharField(null=True, blank=True, max_length=256)
     
+    
+         
 extension_pool.register(AppInfoExtension)
